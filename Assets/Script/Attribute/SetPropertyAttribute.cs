fb26b8a4601e5b4e2bd28c8bc3f467fd6b78ba74
Evil.Ghost <lixiayi@verycd.com>
2015-02-05 14:38:15 +0800

    新增检视器中修改数值调用属性set方法的扩展

---
+++ Assets/Script/Attribute/SetPropertyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Ghost.Attribute
+{
+	public class SetPropertyAttribute : PropertyAttribute
+	{
+		public string name { get; private set; }
+		public bool isDirty { get; set; }
+		
+		public SetPropertyAttribute(string n)
+		{
+			this.name = n;
+		}
+	}
+}
---
+++ Assets/Script/Test/TestSetProperty.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ghost.Attribute;
+
+namespace Ghost.Test
+{
+	public class TestSetProperty : MonoBehaviour {
+
+		[SerializeField, SetProperty("property")]
+		private int property_ = 0;
+
+		public int property
+		{
+			get
+			{
+				return property_;
+			}
+			set
+			{
+				property_ = value;
+				Debug.Log(string.Format("set property: {0}", value));
+			}
+		}
+		
+		// Use this for initialization
+		void Start () {
+			Debug.Log(string.Format("property: {0}", property_));
+		}
+		
+//		// Update is called once per frame
+//		void Update () {
+//			
+//		}
+
+	}
+}
